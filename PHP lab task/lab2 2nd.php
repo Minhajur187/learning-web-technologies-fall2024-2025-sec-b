f123cd2ad18becfdfce2a2b1291d8b0eb4361bff
Minhajur187 <minhajuralamin@gmail.com>
2024-11-17 13:24:10 +0600

    Delete PHP lab task directory

--- PHP lab task/lab2 2nd.php
+++
@@ -1,9 +0,0 @@
-<?php
-$amount = 500;
-$vatpercentage = $amount*0.15;
-$vatAmount = $amount * $vatpercentage;
-$totalAmount = $amount + $vatAmount;
-echo "Original Amount: $" . $amount . "\n";
-echo "VAT (15%): $" . $vatAmount . "\n";
-echo "Total Amount: $" . $totalAmount . "\n";
-?>
