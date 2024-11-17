d429dc8749e55681658fe2143a9205381a990422
Minhajur187 <minhajuralamin@gmail.com>
2024-11-17 13:23:13 +0600

    Add files via upload

---
+++ PHP LAB 3/lab2 3rd.php
@@ -0,0 +1,11 @@
+<?php
+function evenodd($number){
+    if($number%2==0){
+        return "even";
+
+    }else{
+        return "odd"
+    }
+}
+echo "the number is" .evenodd($number);
+?>
---
+++ PHP LAB 3/lap2 triangle.php
@@ -0,0 +1,10 @@
+<?php
+$length;
+$width;
+$area = $length * $width;
+$perimeter = 2 * ($length + $width);
+echo "Area: " . $area."/r/n";
+echo "perimeer: " . $perimeer."/r/n";
+?>
+
+
